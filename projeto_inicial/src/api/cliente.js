f393b4f03876e8be7b142dec02cfa1fd6310faef
felipedotcom <felipe.rodolfo29@gmail.com>
2020-05-27 10:43:57 -0300

    projeto_inicial

---
+++ projeto_inicial/src/api/cliente.js
@@ -0,0 +1,56 @@
+const listarClientes = () => {
+    return fetch('http://localhost:4000/clientes')
+    .then( resposta => { 
+      return resposta.json()
+    })
+    .then( json => { 
+      return json 
+    })
+}
+
+const cadastrarClientes = (nome, cpf) =>  {
+  const json = JSON.stringify({
+    nome: nome,
+    cpf: cpf
+  })
+  return fetch('http://localhost:4000/clientes/cliente',{
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: json
+  })
+  .then( resp => { 
+    return resp.body
+  })
+} 
+
+const deletaCliente = id => {
+  return fetch(`http://localhost:4000/clientes/cliente/${id}`, {
+    method: "DELETE",
+})}
+
+
+const detalhaCliente = id => { 
+  return fetch(`http://localhost:4000/clientes/cliente/${id}`,{
+    method: 'GET'
+  }) 
+  .then(resposta => {
+    return resposta.json()
+  })
+}
+
+const editaCliente = (id, cpf, nome) => {
+  const json = JSON.stringify({
+    nome: nome,
+    cpf: cpf
+  })
+  return fetch(`http://localhost:4000/clientes/cliente/${id}`,{
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: json
+  })
+}
+
---
+++ projeto_inicial/src/componentes/lista/listagem-cliente.js
@@ -0,0 +1,41 @@
+
+
+const removeCliente = (id) => {
+  if(confirm("Deseja deletar o cliente ?")){
+    debugger;
+    deletaCliente(id)
+    window.location.reload()
+  }
+}
+
+const corpoTabela = document.querySelector("[data-conteudo-tabela]");
+
+const exibeCliente = (cpf, nome, id) => {
+    const linha = document.createElement('tr');
+
+    const conteudoLinha = `
+    <td>${cpf}</td>
+    <td>${nome}</td>
+    <button type="button" class="btn btn-danger" onclick="removeCliente(${id})">Excluir</button>
+    <a href="./edita/edita-clientes.html?id=${id}">
+    <button type=""button class="btn btn-info">Editar</button>
+    </a>
+    
+    
+`
+  
+    linha.innerHTML = conteudoLinha;
+    return linha;
+  };
+  
+  listarClientes().then( exibe => {
+  exibe.forEach(indice => {
+    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id))
+  })
+ }
+
+ )
+  
+  
+ 
+
