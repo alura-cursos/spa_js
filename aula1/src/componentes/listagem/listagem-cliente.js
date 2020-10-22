d6a438cdb10ee73a0586802408f26ede2faf83b1
Rômulo Henrique <romulo.henriquemv@gmail.com>
2020-10-22 10:35:14 -0300

    Update listagem-cliente.js

--- aula1/src/componentes/listagem/listagem-cliente.js
+++ aula1/src/componentes/listagem/listagem-cliente.js
@@ -16,7 +16,7 @@ const exibeCliente = (cpf, nome, id) => {
     <td>${cpf}</td>
     <td>${nome}</td>
     <button type="button" class="btn btn-danger" onclick="removeCliente(${id})">Excluir</button>
-    <a href="edita-clientes.html?id=${id}">
+    <a href="./componentes/edita/edita-clientes.html?id=${id}">
     <button type=""button class="btn btn-info">Editar</button>
     </a>
     
